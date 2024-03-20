22dbec2d9ec87a701112d60739d38cbb861a40b5
Jakub Kubera <jakub.kubera@zdt.io>
2024-03-20 14:12:42 +0100

    changed version of engine to 16

--- examples/create_postgresql_database/main.tf
+++ examples/create_postgresql_database/main.tf
@@ -2,7 +2,7 @@ module "Digitalocean_postgresql" {
   source                     = "github.com/opsd-io/terraform-module-digitalocean-postgresql?ref=2-create-postgresql-terraform-module"
   databasename               = "example-postgresql-cluster"
   engine                     = "pg"
-  version_of_engine          = "11"
+  version_of_engine          = "16"
   postgresql_main_size       = "db-s-1vcpu-1gb"
   region                     = "nyc1"
   postgresql_main_node_count = 1
--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "postgresql_main_size" {
 variable "version_of_engine" {
   description = "Engine version used by the cluster."
   type        = number
-  default     = 11
+  default     = 16
 }
 
 variable "region" {
