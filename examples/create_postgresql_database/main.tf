9389d7f58f9fdc1812bf1f5f00b1b71eefc70632
Jakub Kubera <jakub.kubera@zdt.io>
2024-03-20 13:52:57 +0100

    change vars in example

--- examples/create_postgresql_database/main.tf
+++ examples/create_postgresql_database/main.tf
@@ -1,12 +1,12 @@
-module "Digitalocean_mysql" {
-  source                = "github.com/opsd-io/terraform-module-digitalocean-postgresql?ref=2-create-postgresql-terraform-module"
-  databasename          = "example-postgresql-cluster"
-  engine                = "pg"
-  version_of_engine     = "11"
-  mysql_main_size       = "db-s-1vcpu-1gb"
-  region                = "nyc1"
-  mysql_main_node_count = 1
-  common_tags           = ["production"]
-  database_users        = ["Admin", "Mark", "Robert"]
-  firewall_rules        = ["192.168.0.1", "192.168.0.2", "192.168.0.8"]
+module "Digitalocean_postgresql" {
+  source                     = "github.com/opsd-io/terraform-module-digitalocean-postgresql?ref=2-create-postgresql-terraform-module"
+  databasename               = "example-postgresql-cluster"
+  engine                     = "pg"
+  version_of_engine          = "11"
+  postgresql_main_size       = "db-s-1vcpu-1gb"
+  region                     = "nyc1"
+  postgresql_main_node_count = 1
+  common_tags                = ["production"]
+  database_users             = ["Admin", "Mark", "Robert"]
+  firewall_rules             = ["192.168.0.1", "192.168.0.2", "192.168.0.8"]
 }
