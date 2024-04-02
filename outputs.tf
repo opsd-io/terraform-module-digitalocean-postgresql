110fe008206a9e7df8a5b01707cfb7ac25a2fd44
Piotr Stawarski <piotr.stawarski@zdt.io>
2024-04-02 11:58:59 +0200

    formatting

--- outputs.tf
+++ outputs.tf
@@ -20,14 +20,14 @@ output "main_port" {
 
 output "main_uri" {
   description = "The full URI for connecting to the database cluster."
-  value       = digitalocean_database_cluster.main.uri
   sensitive   = true
+  value       = digitalocean_database_cluster.main.uri
 }
 
 output "main_private_uri" {
   description = "Same as uri, but only accessible from resources within the account and in the same region."
-  value       = digitalocean_database_cluster.main.private_uri
   sensitive   = true
+  value       = digitalocean_database_cluster.main.private_uri
 }
 
 output "main_default_database" {
@@ -42,17 +42,16 @@ output "main_default_user" {
 
 output "main_default_user_password" {
   description = "Password for the cluster's default user."
-  value       = digitalocean_database_cluster.main.password
   sensitive   = true
+  value       = digitalocean_database_cluster.main.password
 }
 
 output "firewall_id" {
+  description = "A unique identifier for the firewall."
   value       = digitalocean_database_firewall.main.id
-  description = "A unique identifier for the firewall"
 }
 
 output "user_ids" {
-  description = "A unique identifier for database users"
+  description = "A unique identifier for database users."
   value       = [for id in var.database_users : digitalocean_database_user.main[id].id]
-
 }
