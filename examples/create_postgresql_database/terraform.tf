6061dc8240f71ad6081e3663a3ab4996a3a8ab05
Michal Tomczuk <michal.tomczuk@zdt.io>
2024-09-17 09:01:29 +0200

    bump tool versions

--- examples/create_postgresql_database/terraform.tf
+++ examples/create_postgresql_database/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.5.5"
+  required_version = ">= 1.9.5"
 
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "2.34.1"
+      version = "2.41.0"
     }
   }
 }
